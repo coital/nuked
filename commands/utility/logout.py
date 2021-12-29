208871a808f44d3f7498b5e11725ae95bab564c0
coital <kylieex@protonmail.com>
2021-12-28 23:29:10 -0500

    commit

---
+++ commands/utility/logout.py
@@ -0,0 +1,14 @@
+import discord, datetime, base64, signal
+from discord.ext import commands
+from modules import util
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Logout(bot))
+
+class Logout(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.client = bot
+    @commands.command(aliases=["exit"])
+    async def logout(self, ctx):
+        await ctx.message.delete()
+        signal.raise_signal(signal.SIGINT)
